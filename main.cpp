2ff81ab2daad6ec4711e3beca24186a909c8f388
Shawn White <shawnrat@gmail.com>
2022-01-28 11:03:25 +0000

    Support both POSIX and Windows paths.

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <fstream>
+#include <filesystem>
 #include <chrono>
 #include "SolutionRegistry.h"
 
 std::string get_input(uint id) {
-    auto filename = "Input\\" + std::to_string((id / 10) % 200000) + ".txt";
-    std::ifstream file(filename);
+    std::filesystem::path filepath{"Input/" + std::to_string((id / 10) % 200000) + ".txt"};
+    std::ifstream file(filepath.make_preferred());
     if (!file.is_open())
-        throw std::runtime_error("Failed to open file : " + filename);
+        throw std::runtime_error("Failed to open file : " + filepath.string());
 
     std::string contents;
     file.seekg(0, std::ios::end);
