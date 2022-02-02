4136afead0394c99912c7338e67fe33bacaf6a5c
Shawn White <shawnrat@gmail.com>
2022-02-02 17:28:00 +0000

    Solution 2015052

--- 2015/Solution201505.cpp
+++ 2015/Solution201505.cpp
@@ -51,6 +51,36 @@ SOLUTION(2015, 05, 1) {
     return std::to_string(nice_count);
 }
 
+SOLUTION(2015, 05, 2) {
+    std::stringstream input_stream(input, std::ios_base::in);
+    std::string line;
+
+    unsigned int nice_count = 0;
+    while(std::getline(input_stream, line)) {
+        bool has_repeat = false;
+        bool has_palindrome = false;
+
+        for(int i = 2; i < line.size(); i++) {
+            if (line[i] == line[i - 2])
+                has_palindrome = true;
+
+            if (!has_repeat) {
+                for (int j = 0; j < i - 2; j++) {
+                    if (line[j] == line[i - 1] && line[j + 1] == line[i]) {
+                        has_repeat = true;
+                        break;
+                    }
+                }
+            }
+        }
+
+        if (has_repeat && has_palindrome)
+            nice_count++;
+    }
+
+    return std::to_string(nice_count);
+}
+
 #if TEST
 #include "../SolutionTesting.h"
 
@@ -60,5 +90,12 @@ SOLUTION_TEST(2015, 05, 1, examples) {
     EXPECT_EQ("0", INVOKE_SOLUTION(2015, 05, 1, "jchzalrnumimnmhp"));
     EXPECT_EQ("0", INVOKE_SOLUTION(2015, 05, 1, "haegwjzuvuyypxyu"));
     EXPECT_EQ("0", INVOKE_SOLUTION(2015, 05, 1, "dvszwmarrgswjxmb"));
+}
+
+SOLUTION_TEST(2015, 05, 2, examples) {
+    EXPECT_EQ("1", INVOKE_SOLUTION(2015, 05, 2, "qjhvhtzxzqqjkmpb"));
+    EXPECT_EQ("1", INVOKE_SOLUTION(2015, 05, 2, "xxyxx"));
+    EXPECT_EQ("0", INVOKE_SOLUTION(2015, 05, 2, "uurcxstgmygtbstg"));
+    EXPECT_EQ("0", INVOKE_SOLUTION(2015, 05, 2, "ieodomkazucvgmuy"));
 }
 #endif
